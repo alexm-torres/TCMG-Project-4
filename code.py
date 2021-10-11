d342a4e5a879eb2040f0bfb120e7201d5812e1a8
alexm-torres <alexm.torres@tamu.edu>
2021-10-11 17:31:09 -0500

    Update code.py

--- code.py
+++ code.py
@@ -5,21 +5,39 @@ with open("http_access_log.txt") as file_in:
 
 comp = 0
 dc = 0
-
+wcomp = 0
+wc = 0
+mcomp = ""
+mc = 0
 
 for i in array:
     if i.find("[") != -1:
         dl = i.find("[")
         day = i[dl+1:dl+3]
+        month = i[dl+4:dl+7]
 
         if comp == 0:
             comp = day
+            mcomp = month
             dc += 1
         elif comp == day:
             dc += 1
         else:
-            month = i[dl+4:dl+7]
             year = i[dl+8:dl+12]
+            if mcomp != month:
+                print("\nThere were " + str(mc) + " requests made over the span of " + mcomp + " " + year +".\n")
+                mcomp = month
+                mc = 0
+
             print("There were " + str(dc) + " requests made on " + month + " " + day + ", " + year + ".")
+            wc += dc
+            mc += dc
             comp = day 
             dc = 1
+            wcomp += 1
+            if (wcomp == 7):
+                wcomp = 0
+                print("\nThere were " + str(wc) + " requests made over the span of a week.\n")
+                wc = 0
+
+print("\nThere were " + str(mc) + " requests made over the span of " + mcomp + " " + year +".\n")
